8b47038b6acea96d4496db94653abe0c1701c9e7
dbuhryk <dzmitry.buhryk@gmail.com>
2018-04-17 11:23:55 +0200

    Added file submit dummy function

--- module-web-gateway/app/controllers/Main.scala
+++ module-web-gateway/app/controllers/Main.scala
@@ -1,5 +1,7 @@
 package controllers
 
+import java.nio.file.Paths
+
 import play.api.mvc.{Action, AnyContent, ControllerComponents}
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -11,4 +13,20 @@ class Main(controllerComponents: ControllerComponents)
   def index: Action[AnyContent] = Action.async { implicit request =>
     Future(Ok(views.html.index("Name")))
   }
+
+  def submit = Action(parse.multipartFormData) { request =>
+    //Request[MultipartFormData[TemporaryFile]]
+    request.body.file("file").map { f =>
+      val filename = Paths.get(f.filename).getFileName
+      //f.ref.moveTo(Paths.get(s"$filename"), replace = true)
+      Ok.sendFile(f.ref)
+    }.getOrElse {
+      Redirect(routes.Main.index)
+    }
+  }
 }
+
+//  val fileForm = Form(mapping(
+//
+//  )
+//case class FileSubmitForm(file: File)
